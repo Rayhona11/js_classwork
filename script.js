9842abbd0d85e6e1a8a31213e2e51f3b654b1f73
Rayhona <rayhonamirzoyeva.ru>
2024-01-20 14:41:06 +0500

    kjcikjw

---
+++ script.js
@@ -0,0 +1,22 @@
+ let carModel = "BMW"
+
+ let result = 'I have a car with model ${carModel}'
+ console.log(result);
+
+ // ? Number
+
+ let a = 10 ;
+ let b = "qwertyu";
+
+ // ? Value as a string
+// console.log(a+b);
+
+
+// ? Value as  a number
+//console.log(a-b);
+//console.log(a/b);
+//console.log(a*b);
+
+// ? From string to number
+console.log(Number(a));
+
