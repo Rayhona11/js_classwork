c56189636f3450bc671a32629c5bd31d69beb756
Rayhona <rayhonamirzoyeva.ru>
2024-01-25 19:13:14 +0500

    mhfbjeeuhu

---
+++ script3.js
@@ -0,0 +1,41 @@
+// ? MANTIQIY SHART OPERATORLARI
+
+if (15 < 2) {
+    console.log('Salom');
+} else {
+    console.log("Hayr");
+}
+
+// TERNAR 
+(15 > 8) ? console.log('Salom'): console.log('Hayr');
+
+// SWITCH CASE 
+ let year = 2024
+ switch (year){
+    case 2024:
+        console.log('Joriy yili');
+        break;
+    case 1991:
+    console.log('Mustaqillik');
+       break;
+    case 1992:
+        console.log('Konstitutsiya');
+       break;
+    default:
+        console.log('Boshqa yili');
+        //break
+   }
+
+   // MASALA
+   let = masofa =Number(prompt('Qancha masofa bosib o\'tildi'))
+  if (m <= 55){
+    console.log("Toshkent");
+  } else if(masofa <= 175 && masofa > 55){
+     console.log("Sirdaryo");
+  } else if    (masofa <= 325 && masofa > 175){
+      console.log(Jizzax);
+  }else if (masofa > 325){
+    console.log("Samarqand");
+  }else {
+    console.log("uydasiz");
+  }
